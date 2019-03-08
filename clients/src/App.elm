30dc09a329c7aca10bd827222d45f55f40144667
jrootham <jrootham@gmail.com>
2019-03-08 18:57:12 -0500

    Change skeleton to pick up URL Create member database Start add-member

--- clients/src/App.elm
+++ clients/src/App.elm
@@ -2,24 +2,29 @@ module App exposing (main)
 
 import Array
 import Browser
+import Browser.Navigation as Nav
+import Url
 import Http
 import Html exposing (Html, div, text, h1)
+
 import Types as T
 import FixedStrings as Fs
 import FixedStringsConstants as Fsc
 
 
 main =
-    Browser.document
+    Browser.application
         { init = init
         , update = update
         , subscriptions = subscriptions
         , view = view
+        , onUrlRequest = urlRequest
+        , onUrlChange = urlChange
         }
 
 
-init : String -> ( T.Model, Cmd T.Msg )
-init target =
+init : String -> Url.Url -> Nav.Key -> ( T.Model, Cmd T.Msg )
+init target url key =
     ( { server = target, fixedStrings = Array.empty }, (Fs.fetch target "en") )
 
 
@@ -40,3 +45,11 @@ view model =
     { body = [ text model.server ]
     , title = Fs.get model Fsc.title
     }
+
+urlRequest: Browser.UrlRequest -> T.Msg
+urlRequest request =
+    Debug.todo "url request"
+
+urlChange: Url.Url -> T.Msg
+urlChange url =
+    Debug.todo "url change"
---
+++ members/db/base.sql
@@ -0,0 +1,9 @@
+CREATE TABLE members
+(
+	id SERIAL PRIMARY KEY,
+	user_name TEXT NOT NULL,
+	password TEXT,
+	nonce TEXT
+);
+
+CREATE UNIQUE INDEX user_name ON members(user_name);
