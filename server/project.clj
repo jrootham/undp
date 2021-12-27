3ce51ba00c2fe8a14c9b76b026067d9285fdaebb
jrootham <jrootham@gmail.com>
2021-12-27 16:32:58 -0500

    Stash

--- server/project.clj
+++ server/project.clj
@@ -5,7 +5,7 @@
 	:url "http://www.eclipse.org/legal/epl-v10.html"}
 	:dependencies 
   	[
-  		[org.clojure/clojure "1.10.0"]
+  [org.clojure/clojure "1.10.0"]
 		[ring/ring-core "1.7.1"]
 		[ring/ring-jetty-adapter "1.7.1"]
 		[ring-cors "0.1.13"]
